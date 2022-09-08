1b0f88af7b450c6e94e3e091ff3c540383b3ba4f
Pekka Siiriäinen <pekka.siiriainen@wundermanthompson.com>
2022-09-08 14:56:59 +0300

    Linking seems to work with default browser.

--- src/renderer/components/Post.tsx
+++ src/renderer/components/Post.tsx
@@ -1,14 +1,35 @@
+import { createRef } from 'react'
 import { PostSchema } from 'renderer/types/PollTypes'
 
+const redditBaseUrl = 'https://www.reddit.com'
+
 const Post = (data: PostSchema): JSX.Element => {
-  const fullUrl = `https://www.reddit.com/${data.permalink}`
+  const fullUrl = `${redditBaseUrl}${data.permalink}`
+  const authorUrl = `${redditBaseUrl}/u/${data.author}`
+
+  const cardRef: React.LegacyRef<HTMLDivElement> = createRef()
+
+  const onPostCardClick = (e: React.MouseEvent<HTMLElement>) => {
+    const targetElement = e.target as HTMLElement
+    if (targetElement.tagName.toLowerCase() === 'a') {
+      return
+    }
+    window.open(fullUrl)
+  }
 
   return (
-    <div className="Post">
-      <a href={fullUrl} className="subtle-link" target="_blank">
-        <h3 dangerouslySetInnerHTML={{ __html: data.title }} />
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      </a>
+    <div className="Post" onClick={onPostCardClick} ref={cardRef}>
+      <h3
+        className="post-main-title"
+        dangerouslySetInnerHTML={{ __html: data.title }}
+      />
+      <h4 className="post-author-link-wrapper">
+        Posted by{' '}
+        <a href={authorUrl} target="_blank">
+          /u/{data.author}
+        </a>
+      </h4>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   )
 }
