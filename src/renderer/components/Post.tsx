85198cacccfb7171d1009239312829cb1de79694
Pekka Siiriäinen <pekka.siiriainen@wundermanthompson.com>
2022-09-08 15:38:06 +0300

    Mostly visual stuff.

--- src/renderer/components/Post.tsx
+++ src/renderer/components/Post.tsx
@@ -1,5 +1,6 @@
 import { createRef } from 'react'
 import { PostSchema } from 'renderer/types/PollTypes'
+import { ArrowUp } from 'react-feather' // https://github.com/feathericons/react-feather
 
 const redditBaseUrl = 'https://www.reddit.com'
 
@@ -17,18 +18,44 @@ const Post = (data: PostSchema): JSX.Element => {
     window.open(fullUrl)
   }
 
+  const thumbnailTitle = `Thumbnail for post "${data.title.substring(
+    0,
+    15
+  )}..."`
+
   return (
     <div className="Post" onClick={onPostCardClick} ref={cardRef}>
-      <h3
-        className="post-main-title"
-        dangerouslySetInnerHTML={{ __html: data.title }}
-      />
-      <h4 className="post-author-link-wrapper">
-        Posted by{' '}
-        <a href={authorUrl} target="_blank">
-          /u/{data.author}
-        </a>
-      </h4>
+      <header>
+        <div className="in-row">
+          <h3 className="post-main-title">
+            {decodeURIComponent(data.title)}
+            {!!data.link_flair_text && !data.link_flair_text.includes(':') && (
+              <span className="flair">{data.link_flair_text}</span>
+            )}
+          </h3>
+
+          <div className="votes">
+            <div className="ups">
+              <ArrowUp size={15} color="#67d696" />
+              &nbsp;{Math.max(data.ups, data.score)}
+            </div>
+          </div>
+          <h4 className="post-author-link-wrapper">
+            Posted by{' '}
+            <a href={authorUrl} target="_blank">
+              /u/{data.author}
+            </a>
+          </h4>
+        </div>
+        {data.thumbnail && data.thumbnail.includes('http') && (
+          <img
+            className="post-thumbnail"
+            src={data.thumbnail}
+            alt={thumbnailTitle}
+            title={thumbnailTitle}
+          />
+        )}
+      </header>
       <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   )
